4db7a917077eed9463d366c0a65a98084096e66e
Maria Eduarda Mendes <maria.m10@aluno.ifsc.edu.br>
2024-03-01 11:54:07 -0300

    Atividades - Aula I

---
+++ contaClick/app/src/main/java/com/contaclick/MainActivity.java
@@ -0,0 +1,32 @@
+package com.contaclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i=0;
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tvContador);
+    }
+
+    protected void onStart(){
+        super.onStart();
+        textView.setText("0");
+    }
+
+    public void contar (View v){
+        i++;
+        TextView tv = (TextView) v;
+        //tv.setText("Olá Mundo!");
+        tv.setText(Integer.toString(i));
+    }
+}
