04f85a3f6007e23eefc680cc09f3f447bb2acbbf
Maria Eduarda Mendes <maria.m10@aluno.ifsc.edu.br>
2024-03-19 09:10:43 -0300

    aula I

--- IMC/app/src/main/java/com/example/imc/MainActivity.java
+++ IMC/app/src/main/java/com/example/imc/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.imc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtAltura, txtPeso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtAltura = findViewById(R.id.txtAltura);
+        txtPeso = findViewById(R.id.txtPeso);
+    }
+
+    public void calcularIMC(View view){
+        double altura = Double.parseDouble(txtAltura.getText().toString());
+        double peso = Double.parseDouble(txtPeso.getText().toString());
+
+        double imc = peso/ (altura*altura);
     }
 }
