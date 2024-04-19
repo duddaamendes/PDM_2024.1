5365aaa4c6c38557e57b2e67a03091ac88cc7981
Maria Eduarda Mendes <maria.m10@aluno.ifsc.edu.br>
2024-04-19 12:05:11 -0300

    Aula 19/04

---
+++ listagemAulaIII/app/src/main/java/com/example/listagemaulaiii/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.listagemaulaiii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] planetas = new String[] {"Tera", "Marte", "Jupiter", "Venus", "Netuno", "Saturno"};
+
+    Planet[] planets = new Planet[]{
+            new Planet("Terra", R.drawable.terra),
+            new Planet("Marte", R.drawable.marte),
+            new Planet("Jupiter", R.drawable.jupiter),
+            new Planet("Venus", R.drawable.venus),
+            new Planet("Netuno", R.drawable.netuno),
+            new Planet("Saturno", R.drawable.saturno)
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView lv = findViewById(R.id.listView);
+        AdapterPlaneta adapter = new AdapterPlaneta(
+                this,
+                R.layout.item_lista_planeta,
+                planets
+        );
+        //ArrayAdapter<String>  adapter = new ArrayAdapter<>(
+        //        this,
+        //        android.R.layout.simple_list_item_1,
+        //        android.R.id.text1,
+        //        planetas
+        //);
+        lv.setAdapter(adapter);
+
+
+
+        //adicionando tratamento de evento clique
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("planet", planetas[i]);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+}
