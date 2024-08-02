cd234a369067d57562cf48d01e71a683075556bb
Maria Eduarda Mendes <maria.m10@aluno.ifsc.edu.br>
2024-08-02 12:04:42 -0300

    Aula 02/08

--- BlocoNotas/app/src/main/java/com/example/bloconotas/CadastrarActivity.java
+++ BlocoNotas/app/src/main/java/com/example/bloconotas/CadastrarActivity.java
@@ -3,12 +3,34 @@ package com.example.bloconotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.bloconotas.controller.NotaController;
+import com.example.bloconotas.model.Nota;
 
 public class CadastrarActivity extends AppCompatActivity {
 
+    Button btnSalvar, btnCancelar;
+    EditText edTitulo, edTexto;
+    NotaController notaController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+        btnCancelar = findViewById((R.id.btnCancelar));
+        btnSalvar=findViewById((R.id.btnSalvar));
+        edTexto = findViewById(R.id.edTexto);
+        edTitulo=findViewById(R.id.edTitulo);
+
+        notaController = new NotaController(getApplicationContext());
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Nota nota = new Nota(edTitulo.getText().toString(), edTexto.getText().toString());
+                notaController.cadastrarNota(nota);
+            }
+        });
     }
 }
--- BlocoNotas/app/src/main/java/com/example/bloconotas/MainActivity.java
+++ BlocoNotas/app/src/main/java/com/example/bloconotas/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.bloconotas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.bloconotas.controller.NotaController;
+import com.example.bloconotas.model.Nota;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    NotaController notaController;
+    ArrayList<Nota> notas;
+    ArrayList<String> tituloNotas;
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        notaController = new NotaController(getApplicationContext());
+        fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), CadastrarActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void atualizarLista(){
+        notas = notaController.listarNota();
+        tituloNotas = getTituloNotas(notas);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                tituloNotas);
+        listView.setAdapter(adapter);
+    }
+
+    public ArrayList<String> getTituloNotas(ArrayList<Nota> lnotas){
+        ArrayList<String> result = new ArrayList<>();
+        for (Nota nota: lnotas){
+            result.add(nota.getTitulo());
+        }
+        return result;
     }
 }
